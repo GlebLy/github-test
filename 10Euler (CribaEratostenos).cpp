bb96d2a079e849e29f1e2c550630f3435f984814
GlebLy <gleblyapunov@gmail.com>
2019-01-08 20:27:44 +0300

    problema 10, davno sdelal ne pomnuyu

--- 10Euler (CribaEratostenos).cpp
+++ 10Euler (CribaEratostenos).cpp
@@ -3,56 +3,57 @@
 #include <vector>
 #include "aux.h"
 
-std::vector<int> ubratDelimyeNa(int vi, int j, std::vector<int> v){
-  int i = j;
-  while (i!=v.end()) {
-    if (v[i]%vi==0) {
-      v.erase(v.begin + i);
+long int sumVector(std::vector<int> v) {
+  long int sum = 0;
+  if (v.size()==0) {
+    std::cout << "NULL" << '\n';
+  }else{
+    int i = 0;
+    while (i<v.size()) {
+      sum += v[i];
+      i++;
     }
-    i++;
   }
-  return v;
+  return sum;
 }
 
-std::vector<int> euclidovoSito(int predel){
+std::vector<int> erastotenovoSito(int predel){
   std::vector<int> v;
   if (predel<2) {
-    return NULL;
+    return v;
   }else if (predel == 2) {
-    return v.push_back(predel);
+    v.push_back(predel);
+    return (v);
   }else{
     int i = 1;
+    v.push_back(2);
     while (i<predel) {
-      if (i%2==0) {
+      if (i%2!=0) {
         v.push_back(i);
       }
       i++;
     }
-    while (v[i] < sqrt(predel)) {
-      if (etoFactor(v[i])) {
-        ubratDelimyeNa(v[i], i, v);
+    int j = 0;
+    while (v[j] < sqrt(predel)) {
+      int k = 0;
+      while (k<v.size()) {
+        if (v[k] % v[j] == 0 && !etoFactor(v[k])) {
+          v.erase(v.begin()+k);
+        }
+        k++;
+      }
+        j++;
       }
     }
+    return v;
   }
-}
-
-
-long int summaFak(int predel){
-  int i = 0;
-  long int sum = 0;
-  while (i<predel) {
-    if (etoFactor(i)) {
-      sum+=i;
-    }
-    i++;
-  }
-  return sum;
-}
 
 int main() {
   int predel;
   std::cout << "Ведите предел: " << '\n';
   std::cin >> predel;
-  std::cout << "Сумма факторов до "<< predel<< ": "<< summaFak(predel) << '\n';
+  std::vector<int> v;
+  v = erastotenovoSito(predel);
+  std::cout << "Сумма факторов до "<< predel<< ": "<< sumVector(v) << '\n';
   return 0;
 }
--- aux.h
+++ aux.h
@@ -1,3 +1,4 @@
 bool etoFactor(long long chislo);
 
 bool isPrime(int chislo);
+//void showVector(std::vector<int> v);
