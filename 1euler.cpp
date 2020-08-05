7dc92a6c89226bd4083834a705e4427b4e5679a3
GlebLy <31567935+GlebLy@users.noreply.github.com>
2020-08-05 21:19:07 +0300

    Update 1euler.cpp
    rabotaet

--- 1euler.cpp
+++ 1euler.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
-
+/*wR6-c54-k9b-U2U*/
 int sumD3y5(int n){
   int sum = 0;
   int i = 2;
   while (i < n) {
-    if (i%3==0&&i%5==0 || i%3==0 || i%5==0) {
+    if (i%3==0 && i%5==0 || i%3==0 || i%5==0) {
       sum +=i;
     }
     i++;
